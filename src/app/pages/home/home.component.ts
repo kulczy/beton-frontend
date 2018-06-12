bdb30d09758d53d425cfbde1145a6d201f68ea66
Kulczy <sz.kulczy@gmail.com>
2018-06-12 20:39:56 +0200

    Improve homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,32 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  private loginSubscription: Subscription;
+  private unsubscribe = new Subject<void>();
+  isLoggedIn = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
 
   ngOnDestroy(): void {
-    this.loginSubscription.unsubscribe();
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
   }
 
   login(): void {
-    this.loginSubscription = this.authService
+    this.authService
       .login()
+      .pipe(takeUntil(this.unsubscribe))
       .subscribe((resp: boolean) => {
         if (resp) {
           this.router.navigate(['/app']);
