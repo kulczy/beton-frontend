357ee13bd453d7d7f7e80e98bcecb23b6dffb8a8
Kulczy <sz.kulczy@gmail.com>
2018-06-17 22:18:29 +0200

    Fixed scores edit

--- src/app/pages/team-games-edit/team-games-edit.component.ts
+++ src/app/pages/team-games-edit/team-games-edit.component.ts
@@ -146,10 +146,10 @@ export class TeamGamesEditComponent implements OnInit, OnDestroy {
       id_team: this.teamID,
       player_a: this.formControl.controls.player_a.value,
       player_b: this.formControl.controls.player_b.value,
-      score_a: this.formControl.controls.score_a.value !== null
+      score_a: Number.isInteger(this.formControl.controls.score_a.value)
         ? Number(this.formControl.controls.score_a.value)
         : null,
-      score_b: this.formControl.controls.score_b.value !== null
+      score_b: Number.isInteger(this.formControl.controls.score_b.value)
         ? Number(this.formControl.controls.score_b.value)
         : null
     };
